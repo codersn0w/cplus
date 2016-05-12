fa49e3592ddfcf93b468947166de3857f703512c
川畑拓也 <yosx5230@gmail.com>
2016-05-13 00:48:41 +0900

    Initial Commit

---
+++ cplus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cplus
+//
+//  Created by ThunderRa1n on 2016/05/13.
+//  Copyright © 2016年 ThunderRa1n. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
